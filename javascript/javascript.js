466beb833170910cb35e3b47bd494f986ac0df51
Abc-Xyz09 <sora.mueller@posteo.de>
2026-08-17 19:56:30 +0200

    r

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -109,8 +109,35 @@ navigation = [
 
 function navigationHeader() {
     let navigationHeaderHtml = "";
-    for (const primaryLayer of navigation) {
-             navigationHeaderHtml += generateLayerNavigationHeader(primaryLayer, 0);
+    for (const i of navigation) {
+                navigationHeaderHtml +=  `
+
+<div class="dropdown-layer1">
+    <a href="${i.url}">
+        ${i.title}
+    </a>
+        
+        `;
+        if (i.children) {
+                navigationHeaderHtml += `
+
+<button type ="button" class="toggle-dropdown1-navigation">
+    <span class="icon-arrow">
+        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="26px" height="16.043px" viewBox="57 35.171 26 16.043" enable-background="new 57 35.171 26 16.043" xml:space="preserve">
+            <path d="M57.5,38.193l12.5,12.5l12.5-12.5l-2.5-2.5l-10,10l-10-10L57.5,38.193z"></path>
+        </svg>
+    </span>
+</button>
+
+                `;
+            }
+            navigationHeaderHtml += "</div>"
+        if (i.children) {
+            navigationHeaderHtml += `<div class="dropdown-content-layer2">`
+            navigationHeaderHtml += generateLayerNavigationHeader(i, 1);
+            navigationHeaderHtml += "</div>"
+        }
+
     }
     document.getElementById("dropdown-content-layer1").innerHTML = navigationHeaderHtml;
 
